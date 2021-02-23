406bf811b2b5bfda32cac10aa13ab6e7e44a7789
Andrei Lapicov <53965248+anderson4ik@users.noreply.github.com>
2021-02-23 23:29:44 +0200

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import {Environment} from './interface';
-
-export const environment: Environment = {
-  production: true,
-  apiKey: 'AIzaSyBsOtIUGfkJok8lH_cgUAED8fJ7s-hQPKc',
-  fbDataBaseUrl: 'https://angular-my-blog-default-rtdb.firebaseio.com'
-};
